dcb00460a46bf4d4a2bf8bc5f19c74868dc3b6ab
Phillip Schichtel <phillip@schich.tel>
2023-12-29 21:20:23 +0100

    replace the rspamd with curl

--- Dockerfile
+++ Dockerfile
@@ -9,14 +9,6 @@ LABEL maintainer="Phillip Schichtel <phillip@schich.tel>"
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update \
- && apt-get install --no-install-recommends -y curl gnupg ca-certificates \
- && curl -fsSL "https://rspamd.com/apt-stable/gpg.key" | gpg --batch --yes --dearmor -o "/usr/share/keyrings/rspamd.gpg" \
- && echo "deb [signed-by=/usr/share/keyrings/rspamd.gpg] http://rspamd.com/apt-stable/ ${DEBIAN_CODENAME} main" > /etc/apt/sources.list.d/rspamd.list \
- && apt-get purge -y gnupg curl \
- && apt-get autoremove --purge -y \
- && apt-get update
-
-RUN apt-get update \
  && apt-get install -y --no-install-recommends dovecot-imapd dovecot-lmtpd dovecot-sieve dovecot-managesieved dovecot-solr dovecot-auth-lua lua-socket lua-json rspamd
 
 RUN addgroup --system --gid 5000 vmail \
--- rspamc.sh
+++ rspamc.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
 
+# this is necessary because the process calling this script might not inherit the env from its parent
 . /etc/default/rspamc
 
-if [ -n "${RSPAMD_CONTROLLER_ADDRESS:-}" ]
+if [ -z "${RSPAMD_CONTROLLER_ADDRESS:-}" ]
 then
-    if [ -n "${RSPAMD_CONTROLLER_PASSWORD_FILE:-}" -a -r "${RSPAMD_CONTROLLER_PASSWORD_FILE}" ]
-    then
-        password="$(< "${RSPAMD_CONTROLLER_PASSWORD_FILE}")"
-    else
-        password="${RSPAMD_CONTROLLER_PASSWORD:-}"
-    fi
-    exec /usr/bin/rspamc --connect="${RSPAMD_CONTROLLER_ADDRESS}" --password="${password}" "$@"
-else
-    exec /usr/bin/rspamc "$@"
+    echo "No rspamd controller address given! Use RSPAMD_CONTROLLER_ADDRESS env var!" 1>&2
+    exit 1
 fi
+
+if [ -n "${RSPAMD_CONTROLLER_PASSWORD_FILE:-}" -a -r "${RSPAMD_CONTROLLER_PASSWORD_FILE}" ]
+then
+    password="$(< "${RSPAMD_CONTROLLER_PASSWORD_FILE}")"
+else
+    password="${RSPAMD_CONTROLLER_PASSWORD:-}"
+fi
+
+command=${1?no command given!}
+
+curl -s -H "Password: ${password}" --data-binary @- "http://${RSPAMD_CONTROLLER_ADDRESS}/${command}"
+
